f20b9c50188506df1feff50a8156ee4325dd5f67
Pham Ngoc Phuong Linh <macbookairm1@LinhPham-2.local>
2025-01-17 12:10:59 +0700

    oke

--- streamit_app.py
+++ streamit_app.py
@@ -188,7 +188,7 @@ def app():
             if result['statusCode'] == 200:
                 # Hiển thị toàn bộ result JSON
                 st.header("Result JSON")
-                st.json(result)
+                st.text(result)
 
             else:
                 # Handle lỗi
